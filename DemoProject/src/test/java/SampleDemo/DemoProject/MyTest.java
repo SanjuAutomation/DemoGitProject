0ff5ef0a79e5d5c19aa08dfbf881f96a95fdaa13
sanjay <sanjaysb0404@gmail.com>
2022-06-09 17:16:02 +0530

    added some new files

---
+++ DemoProject/src/test/java/SampleDemo/DemoProject/MyTest.java
@@ -0,0 +1,10 @@
+package SampleDemo.DemoProject;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		
+		String myName = "Vikram";
+	}
+
+}
---
+++ DemoProject/src/test/java/SampleDemo/DemoProject/RegistrationPageTest.java
@@ -0,0 +1,12 @@
+package SampleDemo.DemoProject;
+
+public class RegistrationPageTest {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+
+	}
+
+}
